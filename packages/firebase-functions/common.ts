35c3f44aa9ddcb96c6b0904efe5cf94979be92d0
Osei Fortune <fortune.osei@yahoo.com>
2022-05-29 14:17:34 -0400

    fix(functions): remove deleted method

--- packages/firebase-functions/common.ts
+++ packages/firebase-functions/common.ts
@@ -1,64 +1,41 @@
-import {FirebaseApp} from "@nativescript/firebase-core";
+import { FirebaseApp } from '@nativescript/firebase-core';
 
-export type FunctionsErrorCode =
-  | 'ok'
-  | 'cancelled'
-  | 'unknown'
-  | 'invalid-argument'
-  | 'deadline-exceeded'
-  | 'not-found'
-  | 'already-exists'
-  | 'permission-denied'
-  | 'resource-exhausted'
-  | 'failed-precondition'
-  | 'aborted'
-  | 'out-of-range'
-  | 'unimplemented'
-  | 'internal'
-  | 'unavailable'
-  | 'data-loss'
-  | 'unauthenticated';
-
+export type FunctionsErrorCode = 'ok' | 'cancelled' | 'unknown' | 'invalid-argument' | 'deadline-exceeded' | 'not-found' | 'already-exists' | 'permission-denied' | 'resource-exhausted' | 'failed-precondition' | 'aborted' | 'out-of-range' | 'unimplemented' | 'internal' | 'unavailable' | 'data-loss' | 'unauthenticated';
 
 export interface HttpsCallableOptions {
-  timeout?: number;
+	timeout?: number;
 }
 
 export type HttpsCallableResult = any;
 
-
 export interface HttpsCallable {
-  (data?: any): Promise<HttpsCallableResult>;
+	(data?: any): Promise<HttpsCallableResult>;
 }
-
 
 export enum HttpsErrorCode {
-  OK = 'ok',
-  CANCELLED = 'cancelled',
-  UNKNOWN = 'unknown',
-  INVALID_ARGUMENT = 'invalid-argument',
-  DEADLINE_EXCEEDED = 'deadline-exceeded',
-  NOT_FOUND = 'not-found',
-  ALREADY_EXISTS = 'already-exists',
-  PERMISSION_DENIED = 'permission-denied',
-  UNAUTHENTICATED = 'unauthenticated',
-  RESOURCE_EXHAUSTED = 'resource-exhausted',
-  FAILED_PRECONDITION = 'failed-precondition',
-  ABORTED = 'aborted',
-  OUT_OF_RANGE = 'out-of-range',
-  UNIMPLEMENTED = 'unimplemented',
-  INTERNAL = 'internal',
-  UNAVAILABLE = 'unavailable',
-  DATA_LOSS = 'data-loss'
+	OK = 'ok',
+	CANCELLED = 'cancelled',
+	UNKNOWN = 'unknown',
+	INVALID_ARGUMENT = 'invalid-argument',
+	DEADLINE_EXCEEDED = 'deadline-exceeded',
+	NOT_FOUND = 'not-found',
+	ALREADY_EXISTS = 'already-exists',
+	PERMISSION_DENIED = 'permission-denied',
+	UNAUTHENTICATED = 'unauthenticated',
+	RESOURCE_EXHAUSTED = 'resource-exhausted',
+	FAILED_PRECONDITION = 'failed-precondition',
+	ABORTED = 'aborted',
+	OUT_OF_RANGE = 'out-of-range',
+	UNIMPLEMENTED = 'unimplemented',
+	INTERNAL = 'internal',
+	UNAVAILABLE = 'unavailable',
+	DATA_LOSS = 'data-loss',
 }
 
-
 export interface IFunctions {
-  app: FirebaseApp;
+	app: FirebaseApp;
 
-  httpsCallable(name: string, options?: HttpsCallableOptions): HttpsCallable;
+	httpsCallable(name: string, options?: HttpsCallableOptions): HttpsCallable;
 
-  useEmulator(host: string, port: number);
-
-  useFunctionsEmulatorOrigin(origin: string);
+	useEmulator(host: string, port: number);
 }
--- packages/firebase-functions/index.ios.ts
+++ packages/firebase-functions/index.ios.ts
@@ -1,11 +1,5 @@
-import {
-  FunctionsErrorCode,
-  HttpsCallable,
-  HttpsCallableOptions,
-  HttpsErrorCode,
-  IFunctions
-} from "./common";
-import {deserialize, firebase, FirebaseApp, serialize} from "@nativescript/firebase-core";
+import { FunctionsErrorCode, HttpsCallable, HttpsCallableOptions, HttpsErrorCode, IFunctions } from './common';
+import { deserialize, firebase, FirebaseApp, serialize } from '@nativescript/firebase-core';
 
 let defaultFunctions: Functions;
 
@@ -44,198 +38,194 @@ const fbApp = FirebaseApp;
 Object.defineProperty(fbApp.prototype, 'functions', {
 	value: (regionOrCustomDomain?: string) => {
 		defaultRegionOrCustomDomain = regionOrCustomDomain;
-    if (!defaultFunctions) {
-      defaultFunctions = new Functions();
-    }
+		if (!defaultFunctions) {
+			defaultFunctions = new Functions();
+		}
 		return defaultFunctions;
 	},
 	writable: false,
 });
 
 function errorToCode(error: NSError) {
-  let code = HttpsErrorCode.UNKNOWN;
-  switch (error.code) {
-    case FIRFunctionsErrorCode.OK:
-      code = HttpsErrorCode.OK;
-      break;
-    case FIRFunctionsErrorCode.Cancelled:
-      code = HttpsErrorCode.CANCELLED;
-      break;
-    case FIRFunctionsErrorCode.Unknown:
-      code = HttpsErrorCode.UNKNOWN;
-      break;
-    case FIRFunctionsErrorCode.InvalidArgument:
-      code = HttpsErrorCode.INVALID_ARGUMENT;
-      break;
-    case FIRFunctionsErrorCode.DeadlineExceeded:
-      code = HttpsErrorCode.DEADLINE_EXCEEDED;
-      break;
-    case FIRFunctionsErrorCode.NotFound:
-      code = HttpsErrorCode.NOT_FOUND;
-      break;
-    case FIRFunctionsErrorCode.AlreadyExists:
-      code = HttpsErrorCode.ALREADY_EXISTS;
-      break;
-    case FIRFunctionsErrorCode.PermissionDenied:
-      code = HttpsErrorCode.PERMISSION_DENIED;
-      break;
-    case FIRFunctionsErrorCode.ResourceExhausted:
-      code = HttpsErrorCode.RESOURCE_EXHAUSTED;
-      break;
-    case FIRFunctionsErrorCode.FailedPrecondition:
-      code = HttpsErrorCode.FAILED_PRECONDITION;
-      break;
-    case FIRFunctionsErrorCode.Aborted:
-      code = HttpsErrorCode.ABORTED;
-      break;
-    case FIRFunctionsErrorCode.OutOfRange:
-      code = HttpsErrorCode.OUT_OF_RANGE;
-      break;
-    case FIRFunctionsErrorCode.Unimplemented:
-      code = HttpsErrorCode.UNIMPLEMENTED;
-      break;
-    case FIRFunctionsErrorCode.Internal:
-      code = HttpsErrorCode.INTERNAL;
-      break;
-    case FIRFunctionsErrorCode.Unavailable:
-      code = HttpsErrorCode.UNAVAILABLE;
-      break;
-    case FIRFunctionsErrorCode.DataLoss:
-      code = HttpsErrorCode.DATA_LOSS;
-      break;
-    case FIRFunctionsErrorCode.Unauthenticated:
-      code = HttpsErrorCode.UNAUTHENTICATED;
-      break;
-    default:
-      break;
-  }
+	let code = HttpsErrorCode.UNKNOWN;
+	switch (error.code) {
+		case FIRFunctionsErrorCode.OK:
+			code = HttpsErrorCode.OK;
+			break;
+		case FIRFunctionsErrorCode.Cancelled:
+			code = HttpsErrorCode.CANCELLED;
+			break;
+		case FIRFunctionsErrorCode.Unknown:
+			code = HttpsErrorCode.UNKNOWN;
+			break;
+		case FIRFunctionsErrorCode.InvalidArgument:
+			code = HttpsErrorCode.INVALID_ARGUMENT;
+			break;
+		case FIRFunctionsErrorCode.DeadlineExceeded:
+			code = HttpsErrorCode.DEADLINE_EXCEEDED;
+			break;
+		case FIRFunctionsErrorCode.NotFound:
+			code = HttpsErrorCode.NOT_FOUND;
+			break;
+		case FIRFunctionsErrorCode.AlreadyExists:
+			code = HttpsErrorCode.ALREADY_EXISTS;
+			break;
+		case FIRFunctionsErrorCode.PermissionDenied:
+			code = HttpsErrorCode.PERMISSION_DENIED;
+			break;
+		case FIRFunctionsErrorCode.ResourceExhausted:
+			code = HttpsErrorCode.RESOURCE_EXHAUSTED;
+			break;
+		case FIRFunctionsErrorCode.FailedPrecondition:
+			code = HttpsErrorCode.FAILED_PRECONDITION;
+			break;
+		case FIRFunctionsErrorCode.Aborted:
+			code = HttpsErrorCode.ABORTED;
+			break;
+		case FIRFunctionsErrorCode.OutOfRange:
+			code = HttpsErrorCode.OUT_OF_RANGE;
+			break;
+		case FIRFunctionsErrorCode.Unimplemented:
+			code = HttpsErrorCode.UNIMPLEMENTED;
+			break;
+		case FIRFunctionsErrorCode.Internal:
+			code = HttpsErrorCode.INTERNAL;
+			break;
+		case FIRFunctionsErrorCode.Unavailable:
+			code = HttpsErrorCode.UNAVAILABLE;
+			break;
+		case FIRFunctionsErrorCode.DataLoss:
+			code = HttpsErrorCode.DATA_LOSS;
+			break;
+		case FIRFunctionsErrorCode.Unauthenticated:
+			code = HttpsErrorCode.UNAUTHENTICATED;
+			break;
+		default:
+			break;
+	}
 
-  return code;
+	return code;
 }
 
-export class HttpsError extends Error {
-  readonly code: FunctionsErrorCode;
-  readonly details?: any;
-  readonly native: any;
+const FIRFunctionsErrorDomain = 'com.firebase.functions';
+const FIRFunctionsErrorDetailsKey = 'details';
 
-  constructor(code: FunctionsErrorCode, message: string, details = null, native = null) {
-    super(message);
-    this.code = code;
-    this.details = details;
-    this.native = native;
-  }
+export class HttpsError extends Error {
+	readonly code: FunctionsErrorCode;
+	readonly details?: any;
+	readonly native: any;
+
+	constructor(code: FunctionsErrorCode, message: string, details = null, native = null) {
+		super(message);
+		this.code = code;
+		this.details = details;
+		this.native = native;
+	}
 }
 
 function toHttpsError(error: NSError) {
-  let details = null;
-  if (error.domain == FIRFunctionsErrorDomain) {
-    details = error.userInfo[FIRFunctionsErrorDetailsKey];
-  }
+	let details = null;
+	if (error.domain == FIRFunctionsErrorDomain) {
+		details = error.userInfo[FIRFunctionsErrorDetailsKey];
+	}
 
-  return new HttpsError(
-    errorToCode(error), error.localizedDescription, details, error
-  )
+	return new HttpsError(errorToCode(error), error.localizedDescription, details, error);
 }
 
 export class Functions implements IFunctions {
-  #native: FIRFunctions;
-  #app: FirebaseApp;
+	#native: FIRFunctions;
+	#app: FirebaseApp;
 
-  constructor(app?: FirebaseApp) {
-    if (app?.native) {
-      if(defaultRegionOrCustomDomain){
-        this.#native = isRegion(defaultRegionOrCustomDomain) // Check whether a Region has been set
-          ? FIRFunctions.functionsForAppRegion(app.native, defaultRegionOrCustomDomain)  // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:region:
-          : isCustomDomain(defaultRegionOrCustomDomain) // Check whether using a Custom Domain has been set
-            ? FIRFunctions.functionsForAppCustomDomain(app.native, defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:customdomain:
-            : FIRFunctions.functionsForApp(app.native); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:
-      } else {
-        this.#native = FIRFunctions.functionsForApp(app.native); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:
-      }
-    } else {
-      if(defaultFunctions){
-        return defaultFunctions;
-      }
-      defaultFunctions = this;
-      if(defaultRegionOrCustomDomain){
-        this.#native = isRegion(defaultRegionOrCustomDomain) // Check whether a Region has been set
-          ? FIRFunctions.functionsForRegion(defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforregion:
-          : isCustomDomain(defaultRegionOrCustomDomain) // Check whether using a Custom Domain has been set
-            ? FIRFunctions.functionsForCustomDomain(defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforcustomdomain:
-            : FIRFunctions.functions(); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functions
-      } else {
-        this.#native = FIRFunctions.functions(); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functions
-      }
-    }
-  }
+	constructor(app?: FirebaseApp) {
+		if (app?.native) {
+			if (defaultRegionOrCustomDomain) {
+				this.#native = isRegion(defaultRegionOrCustomDomain) // Check whether a Region has been set
+					? FIRFunctions.functionsForAppRegion(app.native, defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:region:
+					: isCustomDomain(defaultRegionOrCustomDomain) // Check whether using a Custom Domain has been set
+					? FIRFunctions.functionsForAppCustomDomain(app.native, defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:customdomain:
+					: FIRFunctions.functionsForApp(app.native); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:
+			} else {
+				this.#native = FIRFunctions.functionsForApp(app.native); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforapp:
+			}
+		} else {
+			if (defaultFunctions) {
+				return defaultFunctions;
+			}
+			defaultFunctions = this;
+			if (defaultRegionOrCustomDomain) {
+				this.#native = isRegion(defaultRegionOrCustomDomain) // Check whether a Region has been set
+					? FIRFunctions.functionsForRegion(defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforregion:
+					: isCustomDomain(defaultRegionOrCustomDomain) // Check whether using a Custom Domain has been set
+					? FIRFunctions.functionsForCustomDomain(defaultRegionOrCustomDomain) // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functionsforcustomdomain:
+					: FIRFunctions.functions(); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functions
+			} else {
+				this.#native = FIRFunctions.functions(); // @see https://firebase.google.com/docs/reference/ios/firebasefunctions/api/reference/Classes/FIRFunctions#+functions
+			}
+		}
+	}
 
+	httpsCallable(name: string, options?: HttpsCallableOptions): HttpsCallable {
+		const callable = this.native.HTTPSCallableWithName(name);
+		if (typeof options?.timeout === 'number') {
+			callable.timeoutInterval = options.timeout;
+		}
+		return (data: any) => {
+			return new Promise((resolve, reject) => {
+				if (data) {
+					callable.callWithObjectCompletion(serialize(data), (result, error) => {
+						if (error) {
+							reject(toHttpsError(error));
+						} else {
+							resolve(deserialize(result.data));
+						}
+					});
+				} else {
+					callable.callWithCompletion((result, error) => {
+						if (error) {
+							reject(toHttpsError(error));
+						} else {
+							resolve(deserialize(result.data));
+						}
+					});
+				}
+			});
+		};
+	}
 
-  httpsCallable(name: string, options?: HttpsCallableOptions): HttpsCallable {
-    const callable = this.native.HTTPSCallableWithName(name);
-    if (typeof options?.timeout === 'number') {
-      callable.timeoutInterval = options.timeout;
-    }
-    return (data: any) => {
-      return new Promise((resolve, reject) => {
-        if (data) {
-          callable.callWithObjectCompletion(serialize(data), (result, error) => {
-            if (error) {
-              reject(toHttpsError(error))
-            } else {
-              resolve(deserialize(result.data))
-            }
-          })
-        } else {
-          callable.callWithCompletion((result, error) => {
-            if (error) {
-              reject(toHttpsError(error))
-            } else {
-              resolve(deserialize(result.data))
-            }
-          })
-        }
-      })
-    };
-  }
+	useEmulator(host: string, port: number) {
+		this.native.useEmulatorWithHostPort(host, port);
+	}
 
-  useEmulator(host: string, port: number) {
-    this.native.useEmulatorWithHostPort(host, port);
-  }
+	get native() {
+		return this.#native;
+	}
 
-  get native() {
-    return this.#native;
-  }
+	get ios() {
+		return this.native;
+	}
 
-  get ios() {
-    return this.native;
-  }
-
-  useFunctionsEmulatorOrigin(origin: string) {
-    this.native.useFunctionsEmulatorOrigin(origin);
-  }
-
-  get app(): FirebaseApp {
-    if (!this.#app) {
-      // @ts-ignore
-      this.#app = FirebaseApp.fromNative(this.native.app);
-    }
-    return this.#app;
-  }
+	get app(): FirebaseApp {
+		if (!this.#app) {
+			// @ts-ignore
+			this.#app = FirebaseApp.fromNative(this.native.app);
+		}
+		return this.#app;
+	}
 }
 /**
   Check whether a regionOrCustomDomain string is a Region for the http trigger, such as “us-central1”.
   @param regionOrCustomDomain(string): Text to parse
   @return boolean: TRUE if a Region; FALSE if not
   */
-function isRegion(regionOrCustomDomain: string): boolean{
-  const elems = regionOrCustomDomain.split('.');
-  return elems.length === 1 ? true : false;
+function isRegion(regionOrCustomDomain: string): boolean {
+	const elems = regionOrCustomDomain.split('.');
+	return elems.length === 1 ? true : false;
 }
 /**
   Check whether a regionOrCustomDomain string is a Custom Domain for the http trigger, such as “https://mydomain.com”
   @param regionOrCustomDomain(string): Text to parse
   @return boolean: TRUE if a CustomDomain; FALSE if not
   */
-function isCustomDomain(regionOrCustomDomain: string): boolean{
-  return !isRegion(regionOrCustomDomain);
+function isCustomDomain(regionOrCustomDomain: string): boolean {
+	return !isRegion(regionOrCustomDomain);
 }
