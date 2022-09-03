cf47643f510f059e535c361173f59e5628a91e79
Osei Fortune <fortune.osei@yahoo.com>
2022-09-02 23:04:40 -0400

    chore: update vue demo

--- apps/demo-vue/app/app.ts
+++ apps/demo-vue/app/app.ts
@@ -1,11 +1,95 @@
-import Vue from 'nativescript-vue'
-import Home from './components/Home.vue'
+import Vue from 'nativescript-vue';
+import Home from './components/Home.vue';
 
 declare let __DEV__: boolean;
 
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = !__DEV__
+Vue.config.silent = !__DEV__;
+
+import { firebase } from '@nativescript/firebase-core';
+import '@nativescript/firebase-analytics';
+import '@nativescript/firebase-auth';
+import '@nativescript/firebase-crashlytics';
+import '@nativescript/firebase-database';
+import '@nativescript/firebase-dynamic-links';
+import '@nativescript/firebase-firestore';
+import '@nativescript/firebase-functions';
+import '@nativescript/firebase-in-app-messaging';
+import '@nativescript/firebase-installations';
+import '@nativescript/firebase-messaging';
+import '@nativescript/firebase-performance';
+import '@nativescript/firebase-remote-config';
+import '@nativescript/firebase-storage';
+
+import { Admob } from '@nativescript/firebase-admob';
+
+import { AppCheck } from '@nativescript/firebase-app-check-debug';
+import { Application } from '@nativescript/core';
+
+AppCheck.setProviderFactory();
+declare const TNSFirebaseCore;
+firebase()
+	.initializeApp()
+	.then((app) => {
+		firebase().appCheck().activate(true);
+		firebase().crashlytics().setCrashlyticsCollectionEnabled(true);
+	});
+
+Admob.init();
+
+const dynamicLinks = firebase().dynamicLinks();
+
+dynamicLinks.onLink((link) => {
+	console.log('onLink', link);
+});
+
+const messaging = firebase().messaging();
+
+Application.on('launch', (args) => {
+	messaging.onMessage((message) => {
+		console.log('Firebase onMessage', message);
+	});
+
+	messaging.onNotificationTap((message) => {
+		console.log('Firebase onNotificationTap', message);
+	});
+
+	messaging.onToken((token) => {
+		console.log('Firebase onToken', token);
+	});
+});
+
+if (global.isIOS) {
+	messaging
+		.requestPermission()
+		.then(() => {
+			console.log('requestPermission', 'done');
+			messaging.registerDeviceForRemoteMessages().catch((e) => {
+				console.error('registerDeviceForRemoteMessages', e);
+			});
+		})
+		.catch((e) => {
+			console.error('requestPermission', e);
+		});
+}
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
-}).$start()
+	render: (h) => h('frame', [h(Home)]),
+	mounted() {
+		firebase()
+			.messaging()
+			.requestPermission()
+			.then(() => {
+				console.log('requestPermission', 'done');
+				firebase()
+					.messaging()
+					.registerDeviceForRemoteMessages()
+					.catch((e) => {
+						console.error('registerDeviceForRemoteMessages', e);
+					});
+			})
+			.catch((e) => {
+				console.error('requestPermission', e);
+			});
+	},
+}).$start();
--- apps/demo-vue/nativescript.config.ts
+++ apps/demo-vue/nativescript.config.ts
@@ -1,11 +1,12 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.plugindemovue',
+	//id: 'org.nativescript.plugindemovue',
+	id: 'io.github.triniwiz.nativescript.firebasedemo',
 	appResourcesPath: '../../tools/assets/App_Resources',
-  appPath: 'app',
-  android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+	appPath: 'app',
+	android: {
+		v8Flags: '--expose_gc',
+		markingMode: 'none',
+	},
 } as NativeScriptConfig;
