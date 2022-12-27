390d9d85276acdbe9f881ef5d7c79b0df7f7bc93
Eduardo Speroni <edusperoni@gmail.com>
2022-12-27 11:02:44 -0300

    fix(firebase-analytics): consistent handling of Podfile variable $NSFirebaseAnalyticsWithoutAdIdSupport

--- packages/firebase-analytics/hooks/before-checkForChanges.ts
+++ packages/firebase-analytics/hooks/before-checkForChanges.ts
@@ -1,23 +1,37 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 export = function ($logger, $projectData, hookArgs) {
 	const platformName = ((hookArgs.prepareData && hookArgs.prepareData.platform) || (hookArgs.platformData && hookArgs.platformData.normalizedPlatformName) || '').toLowerCase();
 
 	if (platformName === 'ios') {
-		const rootPath = $projectData.projectDir;
-
-		const Podfile = path.join(rootPath, 'platforms', 'ios', 'Podfile');
 		const ResourcePodfile = $projectData.podfilePath;
+		const PluginPodfile = path.join(__dirname, '..', 'platforms', 'ios', 'Podfile');
 
-		if (fs.existsSync(Podfile) && fs.existsSync(ResourcePodfile)) {
-			const podData = fs.readFileSync(Podfile, 'utf8') || '';
+		if (fs.existsSync(PluginPodfile) && fs.existsSync(ResourcePodfile)) {
+			const pluginPodData = fs.readFileSync(PluginPodfile, 'utf8') || '';
 			const resourcePodData = fs.readFileSync(ResourcePodfile, 'utf8');
 
-			// set variable early in the Podfile
+			// set variable in our Podfile to ensure it's picked up correctly
+			if (pluginPodData) {
+				const placeholderRegex = /^(\s*)(.*?)### PLACEHOLDER_FOR_HOOK: \$NSFirebaseAnalyticsWithoutAdIdSupport$/m;
+				const enabledRegex = /^(\s*)\$NSFirebaseAnalyticsWithoutAdIdSupport=true\b/m;
+				const isEnabled = enabledRegex.test(resourcePodData);
 
-			if (podData && resourcePodData && resourcePodData.indexOf('$NSFirebaseAnalyticsWithoutAdIdSupport=true') > -1 && podData.indexOf('$NSFirebaseAnalyticsWithoutAdIdSupport=true') !== 0) {
-				fs.writeFileSync(Podfile, '$NSFirebaseAnalyticsWithoutAdIdSupport=true \n' + podData);
+				const pluginMatch = pluginPodData.match(placeholderRegex);
+				if (pluginMatch) {
+					// this will always be:
+					// `   ### PLACEHOLDER_FOR_HOOK: $NSFirebaseAnalyticsWithoutAdIdSupport`
+					// or `   $NSFirebaseAnalyticsWithoutAdIdSupport=true ### PLACEHOLDER_FOR_HOOK: $NSFirebaseAnalyticsWithoutAdIdSupport`
+					// indentation is always preserved, and an extra space is added after "true"
+					const replacement = isEnabled ? '$NSFirebaseAnalyticsWithoutAdIdSupport=true ' : '';
+					const replacementLine = `${pluginMatch[1]}${replacement}### PLACEHOLDER_FOR_HOOK: $NSFirebaseAnalyticsWithoutAdIdSupport`;
+					if (replacementLine !== pluginMatch[0]) {
+						fs.writeFileSync(PluginPodfile, pluginPodData.replace(placeholderRegex, replacementLine));
+					}
+				} else {
+					$logger.warn(`Could not find NSFirebaseAnalyticsWithoutAdIdSupport placeholder in ${PluginPodfile}`);
+				}
 			}
 		}
 	}
